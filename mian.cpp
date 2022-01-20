6241e2a06ac7a9b4f4ac2581eef78a89a4ed1ae4
wwn <1506514799@qq.com>
2022-01-20 22:23:44 +0800

    add a rotate cube and control it

--- mian.cpp
+++ mian.cpp
@@ -17,17 +17,18 @@
 #include "io/JoyStick.h"
 
 
+unsigned int SCREEN_WIDTH = 800;
+unsigned int SCREEN_HEIGHT = 600;
 
-const int WIDTH = 800;
-const int HEIGHT = 600;
+float x, y, z;
 
 void processInput(GLFWwindow* window);
 void framebufferSizeCB(GLFWwindow* window, int width, int height);
 
-float mix_value = 0.5f;
+float mix_value = 1.0f;
 
 auto transform = glm::mat4(1.0f);
-Joystick main_joy_stick(0);
+//Joystick main_joy_stick(0);
 
 int main()
 {
@@ -36,7 +37,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	auto window = glfwCreateWindow(WIDTH, HEIGHT, "trangle", NULL, NULL);
+	auto window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "trangle", NULL, NULL);
 
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCB);
@@ -45,31 +46,74 @@ int main()
 	glfwSetCursorPosCallback(window, Mouse::cursorPosCallback);
 	glfwSetMouseButtonCallback(window, Mouse::mouseButtonCallback);
 	glfwSetScrollCallback(window, Mouse::mouseWheelCallback);
-
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	glEnable(GL_DEPTH_TEST);
 
 	// shader
 	Shader shader = { "shader/vertex_shader.glsl", "shader/fragment_shader.glsl" };
 	//Shader second_trangle_shader = { "shader/vertex_shader.glsl", "shader/fragment_shader_2.glsl" };
 
+	//float vertices[] = {
+	//	// position					 colors					texture coordinates
+	//	-0.5f,	 -0.5f,	 0.0f,		1.0f,	 1.0f,	 0.5f,	0.0f, 0.0f, // buttom left
+	//	-0.5f,	 0.5f,	 0.0f,		0.5f,	 1.0f,	 0.4f,	0.0f, 1.0f, // top left
+	//	0.5f,	 -0.5f,	 0.0f,		0.8f,	 1.0f,	0.0f,	1.0f, 0.0f, // buttom right
+	//	0.5f,	 0.5f,	 0.0f,		1.0f,	1.0f,	0.5f,	1.0f, 1.0f// top right
+	//};
 	float vertices[] = {
-		// position					 colors					texture coordinates
-		-0.5f,	 -0.5f,	 0.0f,		1.0f,	 1.0f,	 0.5f,	0.0f, 0.0f, // buttom left
-		-0.5f,	 0.5f,	 0.0f,		0.5f,	 1.0f,	 0.4f,	0.0f, 1.0f, // top left
-		0.5f,	 -0.5f,	 0.0f,		0.8f,	 1.0f,	0.0f,	1.0f, 0.0f, // buttom right
-		0.5f,	 0.5f,	 0.0f,		1.0f,	1.0f,	0.5f,	1.0f, 1.0f// top right
+	// position				// uv
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	unsigned int indeices[] = {
-		0, 1, 2,		// first trangle
-		3, 1, 2			// second trangle
-	};
-
-	unsigned int vertex_array_object, vertex_buffer_object, element_buffer_object;
+	unsigned int vertex_array_object, vertex_buffer_object;
 
 	glGenVertexArrays(1, &vertex_array_object);
 	glGenBuffers(1, &vertex_buffer_object);
-	glGenBuffers(1, &element_buffer_object);
+	//glGenBuffers(1, &element_buffer_object);
 
 	// bind vao	
 	glBindVertexArray(vertex_array_object);
@@ -80,36 +124,23 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// setup ebo
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_object);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeices), indeices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_object);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeices), indeices, GL_STATIC_DRAW);
 
 	// set attribute  pointer
 	// positions
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// colors
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
 
 	// textures cooridnates
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// texture
-	/*
-
-				Shder             texture units
-									┌──────┐
-									│      │
-		uniform simple 2D tex_1 ──────┤► 0   ├──────►tex_1
-									├──────┤
-									│      │
-		uniform simple 2D tex_2 ──────┼─►1   ├──────►tex_1
-									├──────┤
-									│  2   │
-									└──────┘
-	*/
 
 	unsigned int texture_1, texture_2;
 
@@ -141,35 +172,28 @@ int main()
 
 
 	auto trans_matrix = glm::mat4(1.0f);
-	trans_matrix = glm::rotate(trans_matrix, glm::radians(45.0f), glm::vec3(0, 0, 1.0f));
+	trans_matrix = glm::rotate(trans_matrix, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	trans_matrix = glm::rotate(trans_matrix, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	shader.active();
 	shader.setMat4("transform", trans_matrix);
 
-	main_joy_stick.update();
 
-	if (main_joy_stick.isPresent())
-	{
-
-		std::cout << main_joy_stick.getName() << std::endl;
-	}
-	else
-		std::cout << "not present" << std::endl;
-
+	// 此项更改的是相机的位置 也就是物体始终在原地自旋转，，但是我们更改了相机的位置
+	x = 0.0f;
+	y = 0.0f;
+	z = 3.0f;
 
 	while (!glfwWindowShouldClose(window))
 	{
 		// render here
 		processInput(window);
-
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// 注意 比如如此的书写 分别激活当前所使用的shader 然后 绑定视图
 		shader.active();
 		shader.setInt("tex_1", 0);
 		shader.setInt("tex_2", 1);
-		//shader.setFloat("mix_value", mix_value);
-		//second_trangle_shader.active();
-		//second_trangle_shader.setMat4("transform", trans_matrix);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture_1);
@@ -177,14 +201,26 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture_2);
 
+		// create transform for screen
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+
+
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f, 0.5f, 0.5f));
+		view = glm::translate(view, glm::vec3(-x, -y, -z));
+		projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT,0.1f,  100.0f);
+
+
 		shader.active();
+		shader.setMat4("model", model);
+		shader.setMat4("view", view);
+		shader.setMat4("projection", projection);
+
 		shader.setFloat("mix_value", mix_value);
-		shader.setMat4("transform", transform);
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		//second_trangle_shader.active();
-		//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(sizeof(unsigned int) * 3));
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -199,62 +235,52 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 
 	// change mix value
-	if (Keyboard::key(GLFW_KEY_UP))
-	{
-		mix_value += 0.01f;
-		if (mix_value > 1)
-		{
-			mix_value = 1.0f;
-		}
-	}
-	if (Keyboard::key(GLFW_KEY_DOWN))
-	{
-		mix_value -= 0.01f;
-		if (mix_value < 0)
-		{
-			mix_value = 0.0f;
-		}
-	}
+	//if (Keyboard::key(GLFW_KEY_UP))
+	//{
+	//	mix_value += 0.01f;
+	//	if (mix_value > 1)
+	//	{
+	//		mix_value = 1.0f;
+	//	}
+	//}
+	//if (Keyboard::key(GLFW_KEY_DOWN))
+	//{
+	//	mix_value -= 0.01f;
+	//	if (mix_value < 0)
+	//	{
+	//		mix_value = 0.0f;
+	//	}
+	//}
 	if (Keyboard::key(GLFW_KEY_W))
 	{
-		transform = glm::translate(transform, glm::vec3(0.0f, 0.01f, 0.0f));
+		y -= 0.01f;
 	}
 	if (Keyboard::key(GLFW_KEY_S))
 	{
-		transform = glm::translate(transform, glm::vec3(0.0f, -0.01f, 0.0f));
+		y += 0.01f;
 	}
 	if (Keyboard::key(GLFW_KEY_A))
 	{
-		transform = glm::translate(transform, glm::vec3(-0.01f, 0.0f, 0.0f));
+		x += 0.01f;
 	}
 	if (Keyboard::key(GLFW_KEY_D))
 	{
-		transform = glm::translate(transform, glm::vec3(0.01f, 0.0f, 0.0f));
+		x -= 0.01f;
 	}
 	if (Keyboard::key(GLFW_KEY_UP))
 	{
-		transform = glm::scale(transform, glm::vec3(0.99f, 0.99f, 0.0f));
+		z -= 0.01f;
 	}
 	if (Keyboard::key(GLFW_KEY_DOWN))
 	{
-		transform = glm::scale(transform, glm::vec3(1.01f, 1.01f, 0.0f));
+		z += 0.01f;
 	}
-	main_joy_stick.update();
-
-
-	//float lx = main_joy_stick.axesState(GLFW_JOYSTICK_AXES_LEFT_STICK_X);
-	//float ly = -main_joy_stick.axesState(GLFW_JOYSTICK_AXES_LEFT_STICK_Y);
-
-	//if (std::abs(lx) > 0.5f)
-	//{
-	//	transform = glm::translate(transform, glm::vec3(lx / 10000, 0.0f, 0.0f));
-	//}
-	//if (std::abs(ly) > 0.5f)
-	//{
-	//	transform = glm::translate(transform, glm::vec3(0.0f, lx / 10000, 0.0f));
-	//}
 }
 void framebufferSizeCB(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+
+	SCREEN_WIDTH = width;
+	SCREEN_HEIGHT = height;
+
 }
--- shader/vertex_shader.glsl
+++ shader/vertex_shader.glsl
@@ -1,19 +1,17 @@
 #version 460 core
 
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec3 color;
-layout (location = 2) in vec2 tex_coordinate;
+layout (location = 1) in vec2 tex_coordinate;
 
-// out color to fragment shader
-out vec3 outColor;
 out vec2 texCoord;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-	gl_Position = transform * vec4(pos, 1.0f);
+	gl_Position = projection * view * model * vec4(pos, 1.0f);
 	// gl_Position =  vec4(pos, 1.0f);
-	outColor = color;
 	texCoord = tex_coordinate;
 }
